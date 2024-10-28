524da7d64dab6ab20bab275bbf3bd00fd814a5c2
markv <mpvismer@users.noreply.github.com>
2024-10-29 00:02:20 +0200

    old changes

--- offscreenrender.py
+++ offscreenrender.py
@@ -14,7 +14,7 @@ from OpenGL.GL import *
 from OpenGL.raw.GL.VERSION import GL_1_1,GL_1_2, GL_3_0
 
 class OffScreenRender(object):
-    
+
     def __init__(self, width=1920, height=1080):
         super(OffScreenRender, self).__init__()
         self._width = width
@@ -23,25 +23,25 @@ class OffScreenRender(object):
         self._render_buf = None
         self._init_fbo()
         self._oldViewPort = (ctypes.c_int*4)*4
-        
+
     def __enter__(self):
         self.activate()
-        
+
     def __exit__(self, type, value, traceback):
         self.deactivate()
         return False
-    
+
     def __del__(self):
         self._cleanup()
-        super(OffScreenRender, self).__del__()      
-       
+        super(OffScreenRender, self).__del__()
+
     def activate(self):
         glBindFramebuffer(GL_FRAMEBUFFER, self._fbo)
         self._oldViewPort = glGetIntegerv(GL_VIEWPORT)
         side = min(self._width, self._height)
         glViewport(int((self._width - side)/2), int((self._height - side)/2), side, side)
-        
-        
+
+
     def deactivate(self):
         glBindFramebuffer(GL_FRAMEBUFFER, 0)
         glViewport(*self._oldViewPort)
@@ -50,32 +50,38 @@ class OffScreenRender(object):
         glReadBuffer(GL_COLOR_ATTACHMENT0)
         width = width is not None or self._width,
         height = height is not None or self._height,
-        glReadPixels(0, 
+        glReadPixels(0,
                      0,
-                     width, 
-                     height, 
-                     GL_BGRA, 
+                     width,
+                     height,
+                     GL_BGRA,
                      #GL_RGBA, alot faster, but incorrect :()
-                     GL_UNSIGNED_BYTE, 
+                     GL_UNSIGNED_BYTE,
                      buf)
                      #outputType=None)
         #GL_1_1.glReadPixels(
-                     
-                     
+        #    x,y,width,height,
+        #    GL_BGRA,
+        #    GL_UNSIGNED_BYTE,
+        #    buf)
+
+
     def get_size(self):
         return self._width*self._height*4
-    
+
     def _init_fbo(self, depth=True):
         fbo = glGenFramebuffers(1)
         self._fbo = fbo
+        #  Could also use GL_READ_FRAMEBUFFER or GL_FRAMEBUFFER for both read
+        #  and draw function calls.
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo)
-        
+
         render_buf = glGenRenderbuffers(1)
         self._render_buf = render_buf
         glBindRenderbuffer(GL_RENDERBUFFER, render_buf)
-        glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, self._width, self._height)    
-        glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, render_buf)        
-        
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, self._width, self._height)
+        glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, render_buf)
+
         if depth:
             depth = glGenRenderbuffers(1)
             self._depth = depth
@@ -83,12 +89,12 @@ class OffScreenRender(object):
             glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, self._width, self._height)
             glBindRenderbuffer(GL_RENDERBUFFER, 0)
             glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth)
-            
+
         assert GL_FRAMEBUFFER_COMPLETE  == glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER)
-        
+
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
-                
-        
+
+
     def _cleanup(self):
         if self._fbo is not None:
             glDeleteFramebuffers(self._fbo)
@@ -96,6 +102,6 @@ class OffScreenRender(object):
         if self._render_buf is not None:
             glDeleteRenderbuffers(self._render_buf)
             self._render_buf = None
-        
-        
-        
+
+
+
